adfc799e842b4edff27b96be50523e2efde41c2d
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2024-11-13 23:59:07 +0100

    likes of posts working.

--- internal/handlers/like.go
+++ internal/handlers/like.go
@@ -47,28 +47,120 @@ func ReactHandler(db *sql.DB, userID int, isUser bool, w http.ResponseWriter, r 
 func updateLikeCount(db *sql.DB, postID int, userID int, reaction string) error {
 
 	// Check if the user has already disliked this post
-	var exists bool
+	var exists int
 	query := `SELECT EXISTS(SELECT 1 FROM likes WHERE post_id = ? AND user_id = ?)`
 	err := db.QueryRow(query, postID, userID).Scan(&exists)
 	if err != nil {
-		return fmt.Errorf("failed to check existing reaction: %w", err)
+		return fmt.Errorf("failed to check existing reaction 1: %w", err)
 	}
-
-	if exists {
-		// Update the reaction if an entry exists
-		updateQuery := `UPDATE likes SET type = ? WHERE post_id = ? AND user_id = ? AND target_type = 'post'`
-		_, err = db.Exec(updateQuery, reaction, postID, userID)
+	if exists == 1 { // user changed the reaction
+		var same int
+		query = `SELECT EXISTS(SELECT 1 FROM likes WHERE post_id = ? AND user_id = ? AND type = ? )`
+		err := db.QueryRow(query, postID, userID, reaction).Scan(&same)
 		if err != nil {
-			return fmt.Errorf("failed to update reaction: %w", err)
+			return fmt.Errorf("failed to check existing reaction 2: %w", err)
 		}
-	} else {
+		// if liked/dislied twise delete
+		// If the user has already reacted the same way (liked/disliked twice), delete the reaction
+		if same == 1 { // user removes the reaction
+			if reaction == "like" {
+				err := removeLike(db, postID)
+				if err != nil {
+					return err
+				}
+			} else if reaction == "dislike" {
+				err := removeDislike(db, postID)
+				if err != nil {
+					return err
+				}
+			}
+			deleteQuery := `DELETE FROM likes WHERE user_id = ? AND post_id = ? AND target_type = 'post'`
+			_, err := db.Exec(deleteQuery, userID, postID)
+			if err != nil {
+				return fmt.Errorf("failed to delete reaction: %w", err)
+			}
+			return nil
+		} else {
+			// here
+			if reaction == "like" {
+				removeDislike(db, postID)
+			} else if reaction == "dislike" {
+				removeLike(db, postID)
+			}
+			//Update the reaction if an entry exists
+			updateQuery := `UPDATE likes SET type = ? WHERE post_id = ? AND user_id = ? AND target_type = 'post'`
+			_, err = db.Exec(updateQuery, reaction, postID, userID)
+			if err != nil {
+				return fmt.Errorf("failed to update reaction 4: %w", err)
+			}
+			if reaction == "like" {
+				err := addLike(db, postID)
+				if err != nil {
+					return err
+				}
+			} else if reaction == "dislike" {
+				err := addDislike(db, postID)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	} else { // first time user reation
 		// Insert a new reaction if no entry exists
 		insertQuery := `INSERT INTO likes (user_id, post_id, target_type, type) VALUES (?, ?, 'post', ?)`
 		_, err = db.Exec(insertQuery, userID, postID, reaction)
 		if err != nil {
-			return fmt.Errorf("failed to insert reaction: %w", err)
+			return fmt.Errorf("failed to insert reaction 5: %w", err)
+		}
+		// Update the like or dislike count
+		if reaction == "like" {
+			err := addLike(db, postID)
+			if err != nil {
+				return err
+			}
+		} else if reaction == "dislike" {
+			err := addDislike(db, postID)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
+	return nil
+}
+
+func addLike(db *sql.DB, postID int) error {
+	updateQuery := `UPDATE posts SET like_count = like_count + 1 WHERE id = ?`
+	_, err := db.Exec(updateQuery, postID)
+	if err != nil {
+		return fmt.Errorf("failed to update reaction: %w", err)
+	}
+	return nil
+}
+
+func addDislike(db *sql.DB, postID int) error {
+	updateQuery := `UPDATE posts SET dislike_count = dislike_count + 1 WHERE id = ?`
+	_, err := db.Exec(updateQuery, postID)
+	if err != nil {
+		return fmt.Errorf("failed to update reaction: %w", err)
+	}
+	return nil
+}
+
+func removeLike(db *sql.DB, postID int) error {
+	updateQuery := `UPDATE posts SET like_count = like_count - 1 WHERE id = ?`
+	_, err := db.Exec(updateQuery, postID)
+	if err != nil {
+		return fmt.Errorf("failed to update reaction: %w", err)
+	}
+	return nil
+}
+
+func removeDislike(db *sql.DB, postID int) error {
+	updateQuery := `UPDATE posts SET dislike_count = dislike_count - 1 WHERE id = ?`
+	_, err := db.Exec(updateQuery, postID)
+	if err != nil {
+		return fmt.Errorf("failed to update reaction: %w", err)
+	}
 	return nil
 }
